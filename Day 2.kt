bf756509fe832a7253466c47bf082377a0e7eae3
Ishan Walia <155130920+ishanwalia7579@users.noreply.github.com>
2024-11-08 19:02:03 +0530

    Day 2.kt

---
+++ Day 2.kt
@@ -0,0 +1,25 @@
+fun main(){
+    println("Hii kotlin")
+    println("hello world")
+    // how to work on print
+    print("hii this is print message")
+    print(" this is next line \n ") // work on \n break a line
+    print("this is next to next line")
+    printUserId()// first 
+    printUserName()//second 
+    printUserAddress()//third
+}
+
+//1
+fun printUserId(){
+    println("printing user id")
+}
+
+//2
+fun printUserName(){
+    println("printing user name")
+}
+//3
+ fun printUserAddress(){
+     println("enter a your address")
+ }
