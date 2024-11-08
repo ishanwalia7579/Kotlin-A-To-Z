9edb4bfa07261f7464fc45d37f732eaa36572b99
Ishan Walia <155130920+ishanwalia7579@users.noreply.github.com>
2024-11-08 19:47:25 +0530

    Create Day 3.kt

---
+++ Day 3.kt
@@ -0,0 +1,25 @@
+// how to used variables :-
+
+fun main(){
+    var name="kotlin"
+    val userId=1111
+    var useraddress="Delhi"
+    
+    println("User name is $name")
+    println(userId)
+    println(useraddress)
+    
+    var a=45
+    var b=89
+    println("value of $a and $b is ${a+b}")
+    
+    a=88
+    b=12
+    println("value of $a and $b is ${a+b}")
+    
+    var num=67
+    var num2=45.9 //type inference 
+    var fatherName="Ravi parsad" // This for string
+    
+    var lastName: string ="rahul sharma" // change for father name 
+}
