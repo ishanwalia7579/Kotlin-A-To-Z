7e73e0e04b19c3e92df6cd53e8c7e3863aed4e73
Ishan Walia <155130920+ishanwalia7579@users.noreply.github.com>
2024-11-09 11:58:22 +0530

    Day 4.kt

---
+++ Day 4.kt
@@ -0,0 +1,36 @@
+                // how to work Data type in kotlin
+fun main()
+{ 
+
+    val number1:Int=34
+    println("number 1 is $number1") // Integer
+    
+    val number2:Byte=12
+    println("number2 is $number2") //Byte
+    
+    val number3:Short=56
+    println("number3 is $number3") //Short
+    
+    val number4:Long =343445664
+    println("number4 is $number4") //Long
+    
+    val marks:Float=22.33f
+    println("This is Float $marks") //Float
+    
+    val marksInComputer:Double =1233.46
+    println("this is for Double $marksInComputer") //Double
+    
+    val ch:Char='A'
+    println("This Character $ch") //Character
+    
+    val isActive:Boolean =true
+    println("this for boolean $isActive") //Boolean
+    
+    
+    val userName:String="Ishan walia"
+    println("This is Name $userName") //userName
+    
+    val message:String="i love you kotlin and i am going to pragram andriod apps"
+     println("this for message $message") //Message
+                  
+}
