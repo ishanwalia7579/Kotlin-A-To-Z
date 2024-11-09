0bf3e65431a3a3f6bcd7bf96d53ac39bab9a0af5
Ishan Walia <155130920+ishanwalia7579@users.noreply.github.com>
2024-11-09 12:00:02 +0530

    Create Day 5.kt

---
+++ Day 5.kt
@@ -0,0 +1,16 @@
+                           // using for array
+ val fruits = arrayOf("Apple", "Banana", "Cherry")
+
+ // Access and print the first fruit
+ println("First fruit: ${fruits[0]}")
+
+ // Modify an element
+ fruits[1] = "Blueberry"
+
+ // Print all fruits
+ for (fruit in fruits) {
+     println(fruit)
+ }
+
+ // Print the size of the array
+ println("Number of fruits: ${fruits.size}")
