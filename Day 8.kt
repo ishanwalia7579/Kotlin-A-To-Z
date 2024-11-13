607556c4e867589c633f127e0c82afb7364b5b20
Ishan Walia <155130920+ishanwalia7579@users.noreply.github.com>
2024-11-13 07:24:29 +0530

    Create Day 8.kt

---
+++ Day 8.kt
@@ -0,0 +1,18 @@
+                    // How to used loop 
+
+fun main(){
+     println("Loops Example")
+     var count=1
+   while (count<=10)
+    {
+        println("ishan walia")
+         count++
+     } // while example 
+
+    
+    for( i in 1..10)
+    {
+       println("for loop")
+    } // for example
+    
+}
