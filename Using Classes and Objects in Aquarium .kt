f12ec53b648d342308260b9937cfadc956bc3a1a
Ishan Walia <155130920+ishanwalia7579@users.noreply.github.com>
2025-05-14 00:14:31 +0530

    Update Using Classes and Objects in Aquarium .kt

--- Using Classes and Objects in Aquarium .kt
+++ Using Classes and Objects in Aquarium .kt
@@ -21,3 +21,31 @@ fun main() {
     buildAquarium()
 }
 output = Width: 20 cm  Length: 100 cm Height: 40 cm 
+
+In buildAquarium(), add code to set the height to 60 and print the changed dimension properties.
+class Aquarium {
+    var width: Int = 20
+    var height: Int = 40
+    var length: Int = 100
+
+    fun printSize() {
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
+    }
+}
+
+fun buildAquarium() {
+    val myAquarium = Aquarium()
+    myAquarium.printSize()      // prints default size
+    myAquarium.height = 60      // changes the height
+    myAquarium.printSize()      // prints updated size
+}
+
+fun main() {
+    buildAquarium()
+}
+
+output :
+Width: 20 cm Length: 100 cm Height: 40 cm 
+Width: 20 cm Length: 100 cm Height: 60 cm 
